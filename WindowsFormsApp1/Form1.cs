a1d484a28187ec98969a2fd069470c338eff4e57
snake97123 <snake12379@yahoo.co.jp>
2021-07-06 14:22:50 +0900

    Mysql.Dataのダウンロード

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,11 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
